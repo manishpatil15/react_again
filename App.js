2d230079a86ab9335d23618d70a8cd96e212d898
Manish Patil <manish.patil2024@outlook.com>
2025-10-16 12:03:47 +0530

    4th

--- App.js
+++ App.js
@@ -13,7 +13,11 @@ const heading = React.createElement(
   React.createElement(
     "div",
     { id: "child2", className: "child2" },
-    React.createElement("h1", {}, "I am h1 tag inside child 2 div")
+    [
+      React.createElement("h1", {}, "I am h1 tag inside child 2 div"),
+      React.createElement("h2", {}, "I am h2 tag inside child 2 div"),
+      React.createElement("h3", {}, "I am h3 tag inside child 2 div")
+    ]
   )
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
